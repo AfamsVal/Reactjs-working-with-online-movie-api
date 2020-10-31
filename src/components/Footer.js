057717457991e4a44fa055e2e68cbda6b361ea88
afams val <myceet1@gmail.com>
2020-10-31 22:51:17 +0100

    more update

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <div className="text-white bg-dark bg-info">
+      <div className="footer-copyright text-center py-4">
+        © 2020 Copyright: <span className="text-warning">Afams Val</span>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
